986647eb5f256206ba23c2c8b7790894667bc73b
akm4545 <akm4545@naver.com>
2025-06-20 17:38:13 +0900

    chapter7

--- chapter7/try-mongo/test-connection.js
+++ chapter7/try-mongo/test-connection.js
@@ -15,12 +15,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // admin DB 인스턴스
+        const adminDB = client.db('test').admin();
+        // 데이터베이스 정보 가져오기
+        const listDatabase = await adminDB.listDatabases();
+
+        console.log(listDatabase);
+
+        return "OK";
+
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
     }
 }
-run().catch(console.dir);
+
+run()
+    .then(console.log)
+    .catch(console.error);
