54a5cc0611d837256494e82596346ee75a10b511
akm4545 <akm4545@naver.com>
2025-07-03 16:05:51 +0900

    chapter9

---
+++ chapter9/hello-nestjs/src/hello.controller.ts
@@ -0,0 +1,13 @@
+// 필요한 함수 임포트
+import { Controller, Get } from "@nestjs/common";
+
+// 컨트롤러 데코레이터
+@Controller
+// 외부에서 사용하므로 export를 붙인다
+export class HelloController {
+    // GET 요청 처리 데코레이터
+    @Get()
+    hello() {
+        return "안녕하세요! NestJS로 만든 첫 애플리케이션입니다.";
+    }
+}
