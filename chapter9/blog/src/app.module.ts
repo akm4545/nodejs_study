1307853cde2b2e63ad3b5640282cc351640885da
akm4545 <akm4545@naver.com>
2025-07-11 18:07:59 +0900

    chapter9

--- chapter9/blog/src/app.module.ts
+++ chapter9/blog/src/app.module.ts
@@ -3,7 +3,9 @@ import { Module } from '@nestjs/common';
 // import { AppService } from './app.service';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
-import { BlogFileRepository } from "./blog.repository";
+import {BlogFileRepository, BlogMongoRepository} from "./blog.repository";
+import {MongooseModule} from "@nestjs/mongoose";
+import {Blog, BlogSchema} from "./blog.schema";
 
 //
 // @Module({
@@ -12,10 +14,17 @@ import { BlogFileRepository } from "./blog.repository";
 //   providers: [AppService],
 // })
 @Module({
-  imports: [],
+  // 몽고디비 연결 설정
+  imports: [
+      MongooseModule.forRoot(
+          'mongodb+srv://<아이디>:<패스워드>@<클러스터정보>/blog',
+      ),
+      // 몽고디비 스키마 설정
+      MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+  ],
   controllers: [BlogController],
   // 프로바이더 설정
-  providers: [BlogService, BlogFileRepository],
+  providers: [BlogService, BlogFileRepository, BlogMongoRepository],
 })
 
 export class AppModule {}
