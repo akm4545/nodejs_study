d274fc4b81df89a4e8eded9af4e3fadcd97280f0
akm4545 <akm4545@naver.com>
2025-07-14 22:28:25 +0900

    chapter10

--- chapter10/config-test/src/app.controller.ts
+++ chapter10/config-test/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+// ConfigService 임포트
+import { ConfigService } from "@nestjs/config";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
+
+  // ConfigService 주입
+  constructor(private configService: ConfigService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    // return this.appService.getHello();
+
+    // configService.get 호출
+    const message = this.configService.get('MESSAGE');
+    return message;
   }
 }
