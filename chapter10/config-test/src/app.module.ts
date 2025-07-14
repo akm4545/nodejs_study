698b32ba7e3d9d321e3cf5d7d32136b625496722
akm4545 <akm4545@naver.com>
2025-07-14 22:13:06 +0900

    chapter10

--- chapter10/config-test/src/app.module.ts
+++ chapter10/config-test/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  // ConfigModule 설정
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
