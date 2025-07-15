df93e1c498c7d58b25f095049ea5881d5e164b86
akm4545 <akm4545@naver.com>
2025-07-15 11:13:54 +0900

    chapter10

--- chapter10/config-test/src/app.module.ts
+++ chapter10/config-test/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { WeatherModule } from './weather/weather.module';
 
 @Module({
   // ConfigModule 설정
   // 전역 모듈 설정 추가
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), WeatherModule],
   controllers: [AppController],
   providers: [AppService],
 })
