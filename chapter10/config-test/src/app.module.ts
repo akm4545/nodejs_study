5ea50f8d8d5bbf88ba1c25cff6afa7c9cdae2a74
akm4545 <akm4545@naver.com>
2025-07-15 11:28:50 +0900

    chapter10

--- chapter10/config-test/src/app.module.ts
+++ chapter10/config-test/src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { WeatherModule } from './weather/weather.module';
 
+// 기동 시 환경 변수 출력
+console.log('env : ' + process.env.NODE_ENV);
+
 @Module({
   // ConfigModule 설정
   // 전역 모듈 설정 추가
