7d4cf7d12191d57d6994fca1ba0856eaaa6f482f
akm4545 <akm4545@naver.com>
2025-07-15 11:36:16 +0900

    chapter10

--- chapter10/config-test/src/app.module.ts
+++ chapter10/config-test/src/app.module.ts
@@ -6,11 +6,17 @@ import { WeatherModule } from './weather/weather.module';
 
 // 기동 시 환경 변수 출력
 console.log('env : ' + process.env.NODE_ENV);
+// 현재 디렉터리 출력
+console.log('current working directory : ' + process.cwd());
 
 @Module({
   // ConfigModule 설정
   // 전역 모듈 설정 추가
-  imports: [ConfigModule.forRoot({ isGlobal: true }), WeatherModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    // 환경 변수 파일 경로 지정
+    envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
+  }), WeatherModule],
   controllers: [AppController],
   providers: [AppService],
 })
