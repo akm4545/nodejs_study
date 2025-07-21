b800e69609570da248a51863ab2ad9252f178026
akm4545 <akm4545@naver.com>
2025-07-21 21:31:51 +0900

    chapter10

--- chapter10/config-test/src/main.ts
+++ chapter10/config-test/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // ConfigService를 app.get()에 추가
+  const configService = app.get(ConfigService);
+  // await app.listen(process.env.PORT ?? 3000);
+  // configService 사용
+  await app.listen(configService.get("SERVER_PORT"));
 }
 bootstrap();
