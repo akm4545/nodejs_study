e2652cb35940fcfcc9fdfb7565ccc84f882e36e8
akm4545 <akm4545@naver.com>
2025-07-21 20:58:29 +0900

    chapter10

--- chapter10/config-test/src/configs/config.ts
+++ chapter10/config-test/src/configs/config.ts
@@ -3,6 +3,9 @@ import local from './local';
 import dev from './dev';
 import prod from './prod';
 
+import { readFileSync } from 'fs';
+import * as yaml from 'js-yaml';
+
 // phase에 NODE_ENV 값 저장
 const phase = process.env.NODE_ENV;
 
@@ -17,8 +20,14 @@ if(phase === 'local') {
     conf = prod;
 }
 
+// YAML 파일 로딩
+const yamlConfig: Record<string, any> = yaml.load(
+    readFileSync(`${process.cwd()}/envs/config.yaml`, 'utf8'),
+) as Record<string, any>;
+
 // common과 conf에서 받은 값을 합쳐서 결괏값으로 주는 함수 반환
 export default () => ({
     ...common,
-    ...conf
+    ...conf,
+    ...yamlConfig, // 기존 설정의 마지막에 덧붙임
 });
