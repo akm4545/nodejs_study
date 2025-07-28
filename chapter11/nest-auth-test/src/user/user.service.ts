6aef703e675fc93d041a6b320faece00fbcd1970
akm4545 <akm4545@naver.com>
2025-07-28 21:29:50 +0900

    chapter11

--- chapter11/nest-auth-test/src/user/user.service.ts
+++ chapter11/nest-auth-test/src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+// 레포지토리 주입 데코레이터
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./user.entity";
+// 레포지토리 임포트
+import { Repository } from "typeorm";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        // 레포지토리 주입
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ) {}
+}
