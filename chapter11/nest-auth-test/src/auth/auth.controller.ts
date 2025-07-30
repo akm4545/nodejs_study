b0d6a0781349d75659f5d23268e12316080f70ce
akm4545 <akm4545@naver.com>
2025-07-30 21:36:47 +0900

    chapter11

--- chapter11/nest-auth-test/src/auth/auth.controller.ts
+++ chapter11/nest-auth-test/src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthService} from "./auth.service";
+import {CreateUserDto} from "../user/user.dto";
 
+// 컨트롤러 생성
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    // AuthService를 주입받음
+    constructor(private authService: AuthService) {}
+
+    @Post('register')
+    // class-validator가 자동으로 유효성 검증
+    async register(@Body() userDto: CreateUserDto) {
+        // authService를 사용해 user 정보 저장
+        return await this.authService.register(userDto);
+    }
+}
