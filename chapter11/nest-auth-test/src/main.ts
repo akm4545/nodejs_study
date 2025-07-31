438ad091751e1300953d7d5abee9f61dec0a9e4e
akm4545 <akm4545@naver.com>
2025-07-31 21:56:20 +0900

    chapter11

--- chapter11/nest-auth-test/src/main.ts
+++ chapter11/nest-auth-test/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 전역 파이프에 validationPipe 객체 추가
   app.useGlobalPipes(new ValidationPipe());
+
+  // 쿠키 파서 설정
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 }
