8a52a7b4c60e1b4a4c2369919e3b3d44373361b1
akm4545 <akm4545@naver.com>
2025-07-31 21:27:27 +0900

    chapter11

--- chapter11/nest-auth-test/src/auth/auth.controller.ts
+++ chapter11/nest-auth-test/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Post, Request, Response} from '@nestjs/common';
 import {AuthService} from "./auth.service";
 import {CreateUserDto} from "../user/user.dto";
 
@@ -13,5 +13,25 @@ export class AuthController {
     async register(@Body() userDto: CreateUserDto) {
         // authService를 사용해 user 정보 저장
         return await this.authService.register(userDto);
+    }
+
+    @Post('login')
+    async login(@Request() req, @Response() res) {
+        // validateUser를 호출해 유저 정보 획득
+        const userInfo = await this.authService.validateUser(
+            req.body.email,
+            req.body.password,
+        );
+
+        // 유저 정보가 있으면 쿠키 정보를 Response에 저장
+        if(userInfo) {
+            res.cookie('login', JSON.stringify(userInfo), {
+                // 브라우저에서 읽을 수 있도록 함
+                httpOnly: false,
+                maxAge: 1000 * 60 * 60 * 24 * 7,
+            });
+        }
+
+        return res.send({ message: 'login success' });
     }
 }
