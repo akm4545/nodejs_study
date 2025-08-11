2732f9814c7af74ce96111586bfaee1ce6d00992
akm4545 <akm4545@naver.com>
2025-08-11 21:52:58 +0900

    chapter12

---
+++ chapter12/nest-auth-test/src/auth/google.strategy.ts
@@ -0,0 +1,41 @@
+import {Injectable} from "@nestjs/common";
+import {PassportStrategy} from "@nestjs/passport";
+import {Profile, Strategy} from "passport-google-oauth20";
+import {UserService} from "../user/user.service";
+
+@Injectable()
+// PassportStrategy(Strategy) 상속
+export class GoogleStrategy extends PassportStrategy(Strategy) {
+    // 생성자
+    constructor(private userService: UserService) {
+        // 부모 클래스의 생성자 호출
+        super({
+            // 클라이언트 ID
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            // 시크릿
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            // 콜백 URL
+            callbackURL: 'http://localhost:3000/auth/google',
+            scope: ['email', 'profile'],
+        } as any);
+    }
+
+    // OAuth 인증이 끝나고 콜백으로 실행되는 메서드
+    async validate(accessToken: string, refreshToken: string, profile: Profile){
+        const {id, name, emails} = profile;
+
+        console.log(accessToken);
+        console.log(refreshToken);
+
+        const providerId = id;
+        if (emails) {
+            const email = emails[0].value;
+
+            if (name) {
+                console.log(providerId, email, name.familyName, name.givenName);
+            }
+        }
+
+        return profile;
+    }
+}
