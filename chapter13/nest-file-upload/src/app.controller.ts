006cf8291711e5e063e612fe63364a44553d4586
akm4545 <akm4545@naver.com>
2025-08-13 14:42:00 +0900

    chapter13

--- chapter13/nest-file-upload/src/app.controller.ts
+++ chapter13/nest-file-upload/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
 import { AppService } from './app.service';
+import {FileInterceptor} from "@nestjs/platform-express";
 
 @Controller()
 export class AppController {
@@ -8,5 +9,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // POST 메서드로 localhost:3000/file-upload 호출 시 동작
+  @Post('file-upload')
+  // 파일 인터셉터
+  @UseInterceptors(FileInterceptor('file'))
+  // 인터셉터에서 준 파일을 받음
+  fileUpload(@UploadedFile() file: Express.Multer.File)
+  {
+    // 텍스트 파일 내용 출력
+    console.log(file.buffer.toString('utf-8'));
+
+    return 'File Upload';
   }
 }
