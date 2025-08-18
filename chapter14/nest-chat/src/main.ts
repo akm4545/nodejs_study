78b93030129d8306a7903a9276ba2a3375233e41
akm4545 <akm4545@naver.com>
2025-08-18 16:55:25 +0900

    chapter14

--- chapter14/nest-chat/src/main.ts
+++ chapter14/nest-chat/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {NestExpressApplication} from "@nestjs/platform-express";
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // NestExpressApplication의 인스턴스 생성
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // 정적 파일 경로 지정
+  app.useStaticAssets(join(__dirname, '..', 'static'));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
