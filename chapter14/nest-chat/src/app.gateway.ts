b3057e04094c60e220d8a762585bf368854edca8
akm4545 <akm4545@naver.com>
2025-08-18 18:40:24 +0900

    chapter14

---
+++ chapter14/nest-chat/src/app.gateway.ts
@@ -0,0 +1,16 @@
+import {SubscribeMessage, WebSocketGateway, WebSocketServer} from "@nestjs/websockets";
+import { Server, Socket } from 'socket.io';
+
+// 웹소켓 서버 설정 데코레이터
+@WebSocketGateway()
+export class ChatGateway {
+    // 웹소켓 서버 인스턴스 선언
+    @WebSocketServer() server: Server;
+    
+    // message 이벤트 구독
+    @SubscribeMessage('message')
+    handleMessage(socket: Socket, data: any): void {
+        // 접속한 클라이언트들에 메시지 전송
+        this.server.emit('message', `client-${socket.id.substring(0, 4)} : ${data}` as any,);
+    }
+}
